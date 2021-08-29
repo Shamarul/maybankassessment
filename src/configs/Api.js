87a572b40c154cb22ad5e1415a5f62ea3080fe75
Shamarul <wanshamarul@yahoo.com>
2021-08-29 19:12:21 +0800

    update readme

--- src/configs/Api.js
+++ src/configs/Api.js
@@ -1,4 +1,4 @@
 module.exports = {
     GOOGLE_MAP: 'https://maps.googleapis.com/maps/api/place',
-    GOOGLE_API_KEY: 'AIzaSyA8J6HyYHMCKm8p8UwwRgIs_IkTlVQWZSs',
+    GOOGLE_API_KEY: '{GOOGLE_TOKEN_HERE}',
 };
